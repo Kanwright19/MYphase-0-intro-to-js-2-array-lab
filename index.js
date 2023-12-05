373dddb923c4eb742805d1635dffb0dc8dbc3908
Kairi Wright <kanwright19@gmail.com>
2023-12-05 17:24:35 -0500

    Implementing Destructive & Nondestructive , Append/Prepend methods aswell

--- index.js
+++ index.js
@@ -1,1 +1,53 @@
 // Write your solution here!
+const cats = [
+    "Milo",
+    "Otis",
+    "Garfield",
+];
+//
+
+function destructivelyAppendCat(catName) {
+  cats.push(catName);
+}  
+
+function destructivelyPrependCat(catName){
+    cats.unshift(catName);
+}
+    
+function destructivelyRemoveLastCat(catName) {
+    cats.pop(catName);
+}
+
+function  destructivelyRemoveFirstCat(catName){
+    cats.shift(catName);
+}
+
+
+
+function appendCat (newCatName) {
+
+const newArr = cats.slice();
+    newArr.push(newCatName);
+        return newArr;
+};
+
+
+function prependCat(newCatName) {
+    
+const newArr = cats.slice();
+    newArr.unshift(newCatName)
+        return newArr
+};
+
+
+const removeLastCat = function () {
+    const newArray = cats.slice();
+    newArray.pop();
+        return newArray;
+};
+
+const removeFirstCat = function () {
+    const newArray = cats.slice();
+    newArray.shift();
+    return newArray;
+};
